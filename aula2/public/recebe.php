c39be02eb62d8395c6f61d8cb97b8556f4ac4ced
Josimar <ciepnejaneja@gmail.com>
2026-08-20 16:23:09 -0300

    Salvo

--- aula2/public/recebe.php
+++ aula2/public/recebe.php
@@ -17,21 +17,3 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
     echo "Número 1: " . $numero1 . "<br>";
     echo "Número 2: " . $numero2;
 } 
-
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $numero1 = $_POST["numero1"];
-    $numero2 = $_POST["numero2"];
-
-    echo "Número 1: " . $numero1 . "<br>";
-    echo "Número 2: " . $numero2;
-} 
-
-
-
-if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    $numero1 = $_GET["numero1"];
-    $numero2 = $_GET["numero2"];
-
-    echo "Número 1: " . $numero1 . "<br>";
-    echo "Número 2: " . $numero2;
-} 
